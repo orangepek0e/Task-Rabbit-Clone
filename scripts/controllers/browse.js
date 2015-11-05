722f3bbcc3d4a660df134e84a47baea771ad8baa
orangepek0e <georgejohnston13@hotmail.com>
2015-11-05 15:58:48 -0400

    Offers added and comments finished

---
+++ scripts/controllers/browse.js
@@ -0,0 +1,86 @@
+'use strict';
+
+app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth, Comment, Offer) {
+
+    $scope.searchTask = '';
+    $scope.tasks = Task.all;
+    $scope.signedIn = Auth.signedIn;
+    $scope.listMode = true;
+
+    $scope.user = Auth.user;
+
+    if($routeParams.taskId) {
+        var task = Task.getTask($routeParams.taskId).$asObject();
+        $scope.listMode = false;
+        setSelectedTask(task);
+    }
+
+    function setSelectedTask(task) {
+        $scope.selectedTask = task;
+
+        if($scope.signedIn()) {
+
+            //Checks if the current user has made an offer on an item
+            Offer.isOffered(task.$id).then(function(data) {
+                $scope.alreadyOffered = data;
+            });
+
+            $scope.isTaskCreator = Task.isCreator;
+            $scope.isOpen = Task.isOpen;
+        }
+
+        $scope.comments = Comment.comments(task.$id);
+
+        $scope.offers = Offer.offers(task.$id);
+
+        $scope.block = false
+
+        $scope.isOfferMaker = Offer.isMaker;
+    }
+
+    $scope.cancelTask = function(taskId) {
+        Task.cancelTask(taskId).then(function() {
+            toaster.pop('success', 'This task was cancelled successfully.');
+        });
+    };
+
+    $scope.addComment = function() {
+        var comment = {
+            content: $scope.content,
+            name: $scope.user.profile.name,
+            gravatar: $scope.user.profile.gravatar
+        };
+
+        console.log('the comment is', comment);
+
+        Comment.addComment($scope.selectedTask.$id, comment).then(function() {
+            $scope.content = '';
+        });
+    };
+
+    $scope.makeOffer = function() {
+        var offer = {
+            total: $scope.total,
+            uid: $scope.user.uid,
+            name: $scope.user.profile.name,
+            gravatar: $scope.user.profile.gravatar
+        };
+
+        Offer.makeOffer($scope.selectedTask.$id, offer).then(function() {
+            toaster.pop('success', 'Your offer has been placed!');
+            $scope.total = '';
+            $scope.block = true;
+            $scope.alreadyOffered = true;
+        });
+    };
+
+    $scope.cancelOffer = function(offerId) {
+        Offer.cancelOffer($scope.selectedTask.$id, offerId).then(function() {
+            toaster.pop('success', 'Your offer has been cancelled.');
+
+            $scope.alreadyOffered = false;
+            $scope.block = false;
+        });
+    };
+
+});
